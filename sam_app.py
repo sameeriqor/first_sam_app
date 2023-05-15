de35a164d286c981308b08563434d459b6b8dbc9
sameeriqor <133002393+sameeriqor@users.noreply.github.com>
2023-05-15 07:16:56 -0400

    Update sam_app.py

--- sam_app.py
+++ sam_app.py
@@ -1,7 +1,4 @@
 import streamlit
-import pandas
-import requests
-fruityvice_response=requests.get("https://fuityvice.com//api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+streamlit.header('Fruityvice fuit advise')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
