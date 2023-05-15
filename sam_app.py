e4ead85e1b9abaecc9742d485738e4aefe5c9765
sameeriqor <133002393+sameeriqor@users.noreply.github.com>
2023-05-15 05:57:05 -0400

    Update sam_app.py

--- sam_app.py
+++ sam_app.py
@@ -1,9 +1,4 @@
-import streamlit
-streamlit.title('My parents new healthy dinner')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
