ccc0ec422f17ebc04ae2c4f5dd19f759b3687d42
sameeriqor <133002393+sameeriqor@users.noreply.github.com>
2023-05-15 07:14:07 -0400

    Update sam_app.py

--- sam_app.py
+++ sam_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response=requests.get"(https://fuityvice.com//api/fruit/watermelon")
+fruityvice_response=requests.get("https://fuityvice.com//api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
