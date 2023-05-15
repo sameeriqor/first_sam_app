8a2c786439563837e2c132c82438a9273b7fdf8d
sameeriqor <133002393+sameeriqor@users.noreply.github.com>
2023-05-15 07:10:50 -0400

    Update sam_app.py

--- sam_app.py
+++ sam_app.py
@@ -3,5 +3,3 @@ import pandas
 iomport requests
 fruityvice_response=requests.get"(https://fuityvice.com//api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
